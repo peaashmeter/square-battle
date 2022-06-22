3619126528ed7f37ef17c6c3e71bd1dcd6850487
peaashmeter <40042317+peaashmeter@users.noreply.github.com>
2022-06-22 14:51:56 +0300

    Revert "removed extra folders"
    This reverts commit f38656da997f6e091338cbf321d9f9675dd48110.

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,20 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import desktop_window
+import path_provider_macos
+import shared_preferences_macos
+import url_launcher_macos
+import window_size
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DesktopWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWindowPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
+}
